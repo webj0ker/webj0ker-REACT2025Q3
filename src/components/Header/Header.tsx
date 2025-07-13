028aa87d4d5dceb54985e6e3026a6393306e4ba4
Stanislav <stanislav.shendryk@gmail.com>
2025-07-13 21:57:51 +0300

    refactor: link for search bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import SearchBar from '../SearchBar';
+import SearchBar from '../SearchBar/SearchBar';
 
 interface HeaderProps {
   onSearch: (searchTerm: string) => void;
