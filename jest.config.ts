b26bba5b215766e1077d0ce8c2118b18e893d697
Stanislav <stanislav.shendryk@gmail.com>
2025-07-19 12:17:50 +0300

    chore: update config for tests

--- jest.config.ts
+++ jest.config.ts
@@ -14,4 +14,20 @@ export default {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.spec.{js,jsx,ts,tsx}',
+    '!src/index.{js,jsx,ts,tsx}',
+    '!src/setupTests.{js,ts}',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+    },
+  },
 };
