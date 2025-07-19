00b3dc249bd6fb27359680a6f7eca57908ba8a33
Stanislav <stanislav.shendryk@gmail.com>
2025-07-19 12:06:31 +0300

    chore: config for tests

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.app.json',
+      },
+    ],
+  },
+  setupFiles: ['whatwg-fetch'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+};
---
+++ src/test/__mocks__/fileMock.js
@@ -0,0 +1,6 @@
+// src/test/__mocks__/fileMock.js
+
+module.exports = {
+  __esModule: true,
+  default: 'test-file-stub',
+};
