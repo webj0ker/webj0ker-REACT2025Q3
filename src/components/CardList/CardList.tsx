05eed32290af5fbaadbc634254ae3b44a124d832
Stanislav <stanislav.shendryk@gmail.com>
2025-07-27 16:10:53 +0300

    refactor: CardList into a functional component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,31 +1,27 @@
-import { Component } from 'react';
 import Card from '../Card/Card';
 
 interface CardListProps {
   results: { name: string; description?: string }[];
 }
 
-class CardList extends Component<CardListProps> {
-  render() {
-    const { results } = this.props;
-    const limitedResults = results.slice(0, 10); // Ограничиваем количество карточек до 10
+const CardList: React.FC<CardListProps> = ({ results }) => {
+  const limitedResults = results.slice(0, 10); // Ограничиваем количество карточек до 10
 
-    return (
-      <div className="card-list">
-        {limitedResults.length === 0 ? (
-          <div>No results</div>
-        ) : (
-          limitedResults.map((result, index) => (
-            <Card
-              key={index}
-              name={result.name}
-              description={result.description}
-            />
-          ))
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-list">
+      {limitedResults.length === 0 ? (
+        <div>No results</div>
+      ) : (
+        limitedResults.map((result, index) => (
+          <Card
+            key={index}
+            name={result.name}
+            description={result.description}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
 export default CardList;
