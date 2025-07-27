be7fb71ffc80b202052c246c4a2af15a4932f60d
Stanislav <stanislav.shendryk@gmail.com>
2025-07-27 16:17:22 +0300

    refactor: Results is now a functional component, removed the Throw Error button

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import SearchResults from '../SearchResults/SearchResults';
 import Spinner from '../Spinner/Spinner';
 import './Results.css';
@@ -7,26 +6,12 @@ interface ResultsProps {
   results: { name: string; description?: string }[];
   error?: string;
   loading: boolean;
-  throwError: () => void;
 }
 
-class Results extends Component<ResultsProps> {
-  render() {
-    const { results, error, loading, throwError } = this.props;
-
-    return (
-      <main>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <SearchResults results={results} error={error} />
-        )}
-        <button className="throw-error-button" onClick={throwError}>
-          Throw Error
-        </button>
-      </main>
-    );
-  }
-}
+const Results: React.FC<ResultsProps> = ({ results, error, loading }) => (
+  <main>
+    {loading ? <Spinner /> : <SearchResults results={results} error={error} />}
+  </main>
+);
 
 export default Results;
