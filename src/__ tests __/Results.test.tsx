39db789454c52bd3d24ecae895a762b6757d61b5
Stanislav <stanislav.shendryk@gmail.com>
2025-08-02 22:20:42 +0300

    chore: install redux toolkit and react-redux

--- src/__ tests __/Results.test.tsx
+++ src/__ tests __/Results.test.tsx
@@ -3,35 +3,16 @@ import { render, screen } from '@testing-library/react';
 import Results from '../components/Results/Results';
 
 test('Показывает ошибку при ошибке API', () => {
-  render(
-    <Results
-      results={[]}
-      loading={false}
-      error="Ошибка"
-      throwError={jest.fn()}
-    />
-  );
+  render(<Results results={[]} loading={false} error="Ошибка" />);
   expect(screen.getByText(/Ошибка/i)).toBeInTheDocument();
 });
 
 test('Показывает ошибку для разных HTTP кодов', () => {
-  render(
-    <Results
-      results={[]}
-      loading={false}
-      error="404 Not Found"
-      throwError={jest.fn()}
-    />
-  );
+  render(<Results results={[]} loading={false} error="404 Not Found" />);
   expect(screen.getByText(/404 Not Found/i)).toBeInTheDocument();
 
   render(
-    <Results
-      results={[]}
-      loading={false}
-      error="500 Internal Server Error"
-      throwError={jest.fn()}
-    />
+    <Results results={[]} loading={false} error="500 Internal Server Error" />
   );
   expect(screen.getByText(/500 Internal Server Error/i)).toBeInTheDocument();
 });
