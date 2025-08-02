cc74c58db25a0d58d74331a9ef651d7d4f3c249b
Stanislav <stanislav.shendryk@gmail.com>
2025-08-02 22:30:50 +0300

    feat: add checkbox to Card and manage selection via Redux

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectItem, unselectItem } from '../../store/selectedSlice';
+import { RootState } from '../../store';
 
 interface CardProps {
   name: string;
   description?: string;
-  onClick?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ name, description, onClick }) => (
-  <div
-    className="card"
-    style={{ cursor: onClick ? 'pointer' : 'default' }}
-    onClick={onClick}
-  >
-    <h3>{name}</h3>
-    {description && <p>{description}</p>}
-  </div>
-);
+const Card: React.FC<CardProps> = ({ name, description }) => {
+  const dispatch = useDispatch();
+  const isSelected = useSelector((state: RootState) =>
+    state.selected.items.some((item) => item.name === name)
+  );
+
+  const handleChange = () => {
+    if (isSelected) {
+      dispatch(unselectItem(name));
+    } else {
+      dispatch(selectItem({ name, description }));
+    }
+  };
+
+  return (
+    <div className="card">
+      <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={handleChange}
+        style={{ marginBottom: 8 }}
+      />
+      <h3>{name}</h3>
+      {description && <p>{description}</p>}
+    </div>
+  );
+};
 
 export default Card;
