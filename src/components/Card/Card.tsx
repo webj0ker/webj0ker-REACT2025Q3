beb632da32993500beb74408f2a33e61377d1307
Stanislav <stanislav.shendryk@gmail.com>
2025-08-02 22:38:23 +0300

    fix: conflict when clicking on card and checkbox

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,15 +6,17 @@ import { RootState } from '../../store';
 interface CardProps {
   name: string;
   description?: string;
+  onClick?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ name, description }) => {
+const Card: React.FC<CardProps> = ({ name, description, onClick }) => {
   const dispatch = useDispatch();
   const isSelected = useSelector((state: RootState) =>
     state.selected.items.some((item) => item.name === name)
   );
 
-  const handleChange = () => {
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
     if (isSelected) {
       dispatch(unselectItem(name));
     } else {
@@ -23,11 +25,16 @@ const Card: React.FC<CardProps> = ({ name, description }) => {
   };
 
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{ cursor: onClick ? 'pointer' : 'default' }}
+      onClick={onClick}
+    >
       <input
         type="checkbox"
         checked={isSelected}
-        onChange={handleChange}
+        onChange={handleCheckboxChange}
+        onClick={(e) => e.stopPropagation()}
         style={{ marginBottom: 8 }}
       />
       <h3>{name}</h3>
