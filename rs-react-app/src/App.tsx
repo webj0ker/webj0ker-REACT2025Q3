0cb1d02f9efe78008c62bfef015e276fbafedc59
Stanislav <stanislav.shendryk@gmail.com>
2025-08-23 12:08:59 +0300

    feat: add buttons for opening modals on main page

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,15 +1,17 @@
-
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [modalType, setModalType] = useState<'uncontrolled' | 'controlled' | null>(null);
 
   return (
     <div className="app-container">
       <h1>Forms Demo</h1>
-     
+      <button onClick={() => setModalType('uncontrolled')}>Open Uncontrolled Form</button>
+      <button onClick={() => setModalType('controlled')}>Open Controlled Form</button>
+      {/* Место для модального окна */}
     </div>
   );
-
 }
 
 export default App;
